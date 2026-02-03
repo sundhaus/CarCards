23b1163d2c0f4a0a33ab366c4a0eccc7f3cb73a4
James Sund <jb@jamessund.com>
2026-02-03 16:15:32 -0500

    Initial Commit

---
+++ Car Collector/Car_CollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_CollectorApp.swift
+//  Car Collector
+//
+//  Created by JB Sund on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Car_CollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car CollectorTests/Car_CollectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Car_CollectorTests.swift
+//  Car CollectorTests
+//
+//  Created by JB Sund on 2/3/26.
+//
+
+import Testing
+@testable import Car_Collector
+
+struct Car_CollectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
