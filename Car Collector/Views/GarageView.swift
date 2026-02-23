33d2c8ba306250b16a0c956cb81ed4b501c084b4
sundhaus <jbsund@users.noreply.github.com>
2026-02-23 17:14:16 +0000

    Driver text: increase leading padding to move away from top edge in portrait

--- Car Collector/Views/GarageView.swift
+++ Car Collector/Views/GarageView.swift
@@ -881,7 +881,7 @@ struct AnyCardDetailsFrontView: View {
                     .rotationEffect(.degrees(-90), anchor: .bottomLeading)
                     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
                     .padding(.bottom, cardHeight * 0.11)
-                    .padding(.leading, cardHeight * 0.03)
+                    .padding(.leading, cardHeight * 0.10)
                 } else {
                     // Vehicle / Location: existing horizontal layout
                     VStack {
